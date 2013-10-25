dbfd7e91c81615bac2df130510675414ffe56f8f
Chris Upjohn <chrisupjohn@gmail.com>
2013-10-25 12:52:19 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studenttransport::Application.config.secret_token = '48625402e92603519bcf41817f4e23ad8df7e90669c7553af630c33a74f5b22fea7f84cba7838be8d07e62b8c4be7051f20ffab885b175e0adf0f574fecf9c20'
